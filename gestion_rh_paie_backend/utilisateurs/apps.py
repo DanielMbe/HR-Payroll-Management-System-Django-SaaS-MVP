4c31d18bebe055fdea0cb0b350ea6dd60bd328ee
Felix Priscille Daniel M’BE <120747471+DanielMbe@users.noreply.github.com>
2026-03-16 12:13:29 +0000

    Fix indentation and formatting in UtilisateursConfig

--- gestion_rh_paie_backend/utilisateurs/apps.py
+++ gestion_rh_paie_backend/utilisateurs/apps.py
@@ -4,5 +4,5 @@ class UtilisateursConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'utilisateurs'
 
-def ready(self):
+    def ready(self):
         import utilisateurs.signals
